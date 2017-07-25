452f4ea223b2ec763f3fec2f965fa5c4eb40a78c
mousa19-meet <mousa19@meet.mit.edu>
2017-07-25 13:45:07 +0300

    Stopping here

---
+++ helloworld.py
@@ -0,0 +1,2 @@
+print("Salamu, Dunia is how you say hello world in swahilli")
+print("i dont want to type all of the languages i already got it :p")
---
+++ specialcharacters.py
@@ -0,0 +1,7 @@
+print(" \"hi\" ")
+print(" \'Hello\' ")
+print(" \\MEET\\Module1\\")
+print("first \n second")
+print("no tab \t  tab")
+#alex
+print ("instructor alex asks,\"what are  you learning today?  \n the students reply , 'we are learning how to print!'\n we can type print ('\\day1\\') to print out \day1\. ")
